d0e33c6f5911479db45fd1ee5953d3190241b7bb
dave morgan <dave@dmorg.org>
2023-10-28 10:42:47 -0600

    second

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
+	import '$lib/styles/global.scss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -7,4 +9,28 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<slot />
+<AppShell slotSidebarLeft="w-0 lg:w-64 bg-surface-500">
+	<svelte:fragment slot="header">
+		<AppBar background="bg-surface-500">
+			<svelte:fragment slot="lead">
+				<button class="lg:hidden header-button btn rounded-full bg-tertiary-400 shadow-lg">
+					<span>
+						<svg viewBox="0 0 100 100" class=" w-5 h-5">
+							<rect width="150" height="20" />
+							<rect y="40" width="150" height="20" />
+							<rect y="80" width="150" height="20" />
+						</svg>
+					</span>
+				</button>
+			</svelte:fragment>
+			<svelte:fragment slot="trail"
+				><div
+					class="header-title rounded-full font-bold text-black py-2 px-6 bg-tertiary-400 shadow-lg"
+				>
+					the-numbers-are-in
+				</div></svelte:fragment
+			>
+		</AppBar>
+	</svelte:fragment>
+	<slot /></AppShell
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
 </div>
